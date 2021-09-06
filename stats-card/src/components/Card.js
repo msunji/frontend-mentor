528fecc2cd4cd1cc02ba5e99107a3537fe7a338c
Marge <marge.consunji@gmail.com>
2021-09-06 11:51:35 +0800

    Made components for Card itself and stats in card

---
+++ stats-card/src/components/Card.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+  width: 90%;
+  max-width: 1440px;
+  background-color: var(--primary-desat-blue);
+  border-radius: 8px;
+`;
---
+++ stats-card/src/components/Stats.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Stats = styled.div`
+  padding: 3rem 2rem;
+  display: flex;
+  flex-flow: column wrap;
+
+  div {
+    flex: 1;
+  }
+`;
