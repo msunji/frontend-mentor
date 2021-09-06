6f72d3c99cc86c71078ee1dd5748c59fe17675f5
Marge <marge.consunji@gmail.com>
2021-09-06 14:59:59 +0800

    Added another media query for stats component flex direction

--- stats-card/src/styles/globalStyle.js
+++ stats-card/src/styles/globalStyle.js
@@ -1,5 +1,5 @@
-import reset from 'styled-reset';
-import { createGlobalStyle } from 'styled-components';
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
     ${reset}
@@ -12,6 +12,8 @@ const GlobalStyle = createGlobalStyle`
         --neutral-white-headings: hsla(0, 0%, 100%, 0.6);
         --font-inter: 'Inter', sans-serif;
         --font-lexend: 'Lexend Deca', sans-serif;
+        --padding-x: 2rem;
+        --padding-y: 2.5rem;
     }
     *,
     *:before,
@@ -47,6 +49,57 @@ const GlobalStyle = createGlobalStyle`
         font-size: 2em;
         font-weight: 700;
         line-height: 1.2;
+    }
+
+    // Media Queries
+    @media screen and (min-width: 760px) {
+        :root {
+            --padding-y: 5rem;
+            --padding-x: 4rem;
+        }
+
+        .card {
+            flex-direction: row-reverse;
+            
+            .card-content {
+                padding-top: var(--padding-y);
+                padding-bottom: var(--padding-y);
+                padding-left: var(--padding-x);
+                padding-right: var(--padding-x);
+                flex: 0 0 50%;
+                min-width: 0;
+            }
+        }
+        .header-img {
+            border-radius: 0 8px 8px 0;
+            flex: 0 0 50%;
+            width: 50%;
+            height: auto;
+            background: url("/assets/img/image-header-desktop.jpg") hsl(277, 64%, 61%);
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+        .card-text {
+            text-align: left;
+
+            p {
+                padding-bottom: calc
+            }
+        }
+        h1 {
+            font-size: 2.1em;
+        }
+    }
+    @media screen and (min-width: 980px) {
+        .card-stats {
+            flex-direction: row;
+                .stat {
+                    text-align: left;
+                    :not(:last-of-type) {
+                        padding-bottom: 0;
+                    }
+                }
+        }
     }
 `;
 
