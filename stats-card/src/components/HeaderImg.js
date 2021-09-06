3e34b4580df5d4e1f0bb3f980064f7ee5ccc3033
Marge <marge.consunji@gmail.com>
2021-09-06 14:01:44 +0800

    Show bg image, add border radius to top left and right corners of image

---
+++ stats-card/src/components/HeaderImg.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const HeaderImg = styled.div`
+  background-color: var(--primary-violet);
+  background: url("/assets/img/image-header-mobile.jpg");
+  background-size: cover;
+  height: 200px;
+  width: 100%;
+  max-height: 100%;
+  overflow: hidden;
+  border-radius: 8px 8px 0 0;
+`;
