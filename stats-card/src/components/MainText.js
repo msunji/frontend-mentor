622128f4561c25dc0d85f0dce62f9f113f8d031d
Marge <marge.consunji@gmail.com>
2021-09-06 12:55:19 +0800

    Added bottom margin to h1

--- stats-card/src/components/MainText.js
+++ stats-card/src/components/MainText.js
@@ -1,8 +1,10 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const MainText = styled.div`
+  text-align: center;
   h1 {
     color: var(--neutral-white);
+    margin-bottom: 1.5rem;
   }
   .highlight {
     color: var(--primary-violet);
