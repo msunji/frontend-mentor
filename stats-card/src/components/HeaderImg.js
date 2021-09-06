f7bdef03f8fc3a9cea0482b21d5f819d6432169b
Marge <marge.consunji@gmail.com>
2021-09-06 14:04:00 +0800

    Add coloured overlay

--- stats-card/src/components/HeaderImg.js
+++ stats-card/src/components/HeaderImg.js
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 
 export const HeaderImg = styled.div`
   background-color: var(--primary-violet);
-  background: url("/assets/img/image-header-mobile.jpg");
+  background: url("/assets/img/image-header-mobile.jpg") hsl(277, 64%, 61%);
   background-size: cover;
   height: 200px;
   width: 100%;
   max-height: 100%;
   overflow: hidden;
   border-radius: 8px 8px 0 0;
+  background-blend-mode: multiply;
 `;
