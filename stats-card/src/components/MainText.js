cc6169363440fe22d44cedf0f27c544bf16d5e74
Marge <marge.consunji@gmail.com>
2021-09-06 12:52:41 +0800

    Added text and highlight for main card text blurb

---
+++ stats-card/src/components/MainText.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const MainText = styled.div`
+  h1 {
+    color: var(--neutral-white);
+  }
+  .highlight {
+    color: var(--primary-violet);
+  }
+`;
