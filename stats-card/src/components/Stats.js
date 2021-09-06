b8b3504d4628b5ebd83d2321593719729fbcf463
Marge <marge.consunji@gmail.com>
2021-09-06 13:02:46 +0800

    Added more spacing between stats numbers

--- stats-card/src/components/Stats.js
+++ stats-card/src/components/Stats.js
@@ -5,7 +5,23 @@ export const Stats = styled.div`
   display: flex;
   flex-flow: column wrap;
 
-  div {
+  .stat {
     flex: 1;
+    text-align: center;
+
+    :not(:last-of-type) {
+      padding-bottom: 1.8rem;
+    }
+
+    h3 {
+      color: var(--neutral-white);
+      font-size: 2em;
+      font-weight: 700;
+      line-height: 1.2;
+    }
+    p {
+      color: var(--neutral-white-headings);
+      text-transform: uppercase;
+    }
   }
 `;
