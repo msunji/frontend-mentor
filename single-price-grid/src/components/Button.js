33309c3540aacda22767d2ca46219dad889ef860
Marge <marge.consunji@gmail.com>
2021-09-07 17:10:54 +0800

    Added really simple hover effect

--- single-price-grid/src/components/Button.js
+++ single-price-grid/src/components/Button.js
@@ -9,5 +9,10 @@ export const Button = styled.button`
   font-weight: 700;
   font-size: 1.01em;
   padding: 0.8rem 3rem;
-  box-shadow: 1px 3px 16px 0px rgb(2 110 194 / 20%);
+  box-shadow: 1px 8px 33px -7px rgba(39, 69, 68, 0.43);
+  transition: background 0.5s ease;
+
+  &:hover {
+    background: #b1bc33;
+  }
 `;
