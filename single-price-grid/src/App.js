8ce00165d059719b3396e40fe497da435b911559
Marge <marge.consunji@gmail.com>
2021-09-07 16:03:58 +0800

    Added text for 'Why Us' section

--- single-price-grid/src/App.js
+++ single-price-grid/src/App.js
@@ -27,7 +27,18 @@ function App() {
             <p>Full access for less than $1 a day</p>
             <Button>Sign Up</Button>
           </section>
-          <section></section>
+          <section className="about">
+            <h2>Why Us</h2>
+            <ul>
+              <li>Tutorials by industry experts</li>
+              <li>Peer &amp; expert code review</li>
+              <li>Coding exercises</li>
+              <li>Access to our GitHub repos</li>
+              <li>Community forum</li>
+              <li>Flashcard decks</li>
+              <li>New videos every week</li>
+            </ul>
+          </section>
         </PriceGrid>
       </main>
       <Footer>
