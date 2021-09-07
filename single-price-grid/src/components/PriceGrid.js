0f7acfc2cf36546358531c714a517d1f0a2b2e34
Marge <marge.consunji@gmail.com>
2021-09-07 15:15:51 +0800

    Added styling for first chunk of text

--- single-price-grid/src/components/PriceGrid.js
+++ single-price-grid/src/components/PriceGrid.js
@@ -1,3 +1,28 @@
 import styled from "styled-components";
 
-export const PriceGrid = styled.div``;
+export const PriceGrid = styled.div`
+  width: 85%;
+  max-width: 1000px;
+  background-color: white;
+  color: var(--greyish-blue);
+  margin: 2rem 0;
+  border-radius: 8px;
+
+  section {
+    padding: var(--padding-y) var(--padding-x);
+  }
+
+  h1 {
+    color: var(--cyan);
+    font-weight: 700;
+    font-size: 1.3em;
+    margin-bottom: 1rem;
+  }
+
+  .highlight {
+    color: var(--bright-yellow);
+    font-weight: 700;
+    line-height: 1.4;
+    margin-bottom: 1rem;
+  }
+`;
