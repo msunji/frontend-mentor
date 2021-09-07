cc6ae45be9e846440b785b2e5051522cfa31ab84
Marge <marge.consunji@gmail.com>
2021-09-07 15:16:04 +0800

    Added text for the first box of the price grid

--- single-price-grid/src/App.js
+++ single-price-grid/src/App.js
@@ -6,7 +6,21 @@ function App() {
     <>
       <GlobalStyle />
       <main>
-        <PriceGrid>test</PriceGrid>
+        <PriceGrid>
+          <section>
+            <h1>Join our community</h1>
+            <p className="highlight">
+              30-day, hassle-free money back guarantee
+            </p>
+            <p>
+              Gain access to our full library of tutorials along with expert
+              code reviews. Perfect for any developers who are serious about
+              honing their skills.
+            </p>
+          </section>
+          <section></section>
+          <section></section>
+        </PriceGrid>
       </main>
       <Footer>
         <p class="attribution">
