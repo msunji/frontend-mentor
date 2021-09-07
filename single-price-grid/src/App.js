4cbfa7a07cc3480268bce76fd2e6ced91c8a15ec
Marge <marge.consunji@gmail.com>
2021-09-07 14:57:10 +0800

    Added Footer component

--- single-price-grid/src/App.js
+++ single-price-grid/src/App.js
@@ -1,7 +1,29 @@
+import GlobalStyle from "./styles/globalStyle";
+import { PriceGrid, Footer } from "./components";
+
 function App() {
   return (
     <>
-      <main>Test</main>
+      <GlobalStyle />
+      <main>
+        <PriceGrid>test</PriceGrid>
+      </main>
+      <Footer>
+        <p class="attribution">
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . <br />
+          Coded by{" "}
+          <a href="https://github.com/msunji/frontend-mentor">Marge Consunji</a>
+          .
+        </p>
+      </Footer>
     </>
   );
 }
