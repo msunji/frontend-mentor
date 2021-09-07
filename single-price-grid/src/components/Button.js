e5f9faa66c023332e8574c41f70433123f44e85a
Marge <marge.consunji@gmail.com>
2021-09-07 15:48:09 +0800

    Added border radius styling, background colour and vertical padding

--- single-price-grid/src/components/Button.js
+++ single-price-grid/src/components/Button.js
@@ -3,4 +3,10 @@ import styled from "styled-components";
 export const Button = styled.button`
   background: var(--bright-yellow);
   color: white;
+  width: 100%;
+  border: none;
+  border-radius: 8px;
+  font-weight: 700;
+  font-size: 1.01em;
+  padding: 0.8rem 3rem;
 `;
