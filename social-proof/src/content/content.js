03c76dff45c71c4ecd5214a3f20737ce3322fc10
Marge <marge.consunji@gmail.com>
2021-09-08 13:39:30 +0800

    Made objects for review and testimonial text

---
+++ social-proof/src/content/content.js
@@ -0,0 +1,35 @@
+export const RatingsData = [
+  {
+    stars: 5,
+    rating: "Rated 5 stars in Reviews",
+  },
+  {
+    stars: 5,
+    rating: "Rated 5 stars in Report Guru",
+  },
+  {
+    stars: 5,
+    rating: "Rated 5 stars in BestTech",
+  },
+];
+
+export const TestimonialsData = [
+  {
+    name: "Colton Smith",
+    title: "Verified Buyer",
+    testimonial:
+      '"We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time.Excellent!"',
+  },
+  {
+    name: "Irene Roberts",
+    title: "Verified Buyer",
+    testimonial:
+      '"Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery."',
+  },
+  {
+    name: "Anne Wallace",
+    title: "Verified Buyer",
+    testimonial:
+      '"Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!"',
+  },
+];
