52ca399f51495aeaa78385f6c208fe10f6e6eb5b
Marge <marge.consunji@gmail.com>
2021-09-08 14:18:07 +0800

    Made a grid component

--- social-proof/src/components/Grid.js
+++ social-proof/src/components/Grid.js
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
 export const Grid = styled.main`
+  width: 85%;
+  max-width: 900px;
+  // border: 1px solid red;
+  margin: calc(3 * var(--margin-y)) auto;
   display: grid;
 `;
