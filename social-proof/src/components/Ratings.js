22552ee250f1e32197c9e5bb79900502a6ae8644
Marge <marge.consunji@gmail.com>
2021-09-08 14:41:11 +0800

    Rendered  N number of stars

--- social-proof/src/components/Ratings.js
+++ social-proof/src/components/Ratings.js
@@ -2,19 +2,24 @@ import styled from "styled-components";
 import { RatingsData } from "../content/content";
 import { ReactComponent as Star } from "../assets/img/icon-star.svg";
 
-const Rating = styled.div``;
+const Rating = styled.div`
+  background: var(--grey-magenta-light);
+`;
 
 export const Ratings = () => {
   console.log(RatingsData);
   return (
     <section>
-      {RatingsData.map(({ stars, rating }) => (
-        <Rating>
+      {RatingsData.map(({ stars, rating, id }) => (
+        <Rating key={id}>
+          <div>
+            {[...Array(stars)].map((e, index) => (
+              <Star key={index} />
+            ))}
+          </div>
           <p>{rating}</p>
         </Rating>
       ))}
-
-      <Star />
     </section>
   );
 };
