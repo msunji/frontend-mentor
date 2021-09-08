57d42eb7572429379e59f231119072d125d7079b
Marge <marge.consunji@gmail.com>
2021-09-08 15:54:20 +0800

    Set grid template areas, added vertical padding to sections in grid

--- social-proof/src/components/Grid.js
+++ social-proof/src/components/Grid.js
@@ -3,8 +3,15 @@ import styled from "styled-components";
 export const Grid = styled.main`
   width: 85%;
   max-width: 900px;
-  // border: 1px solid red;
   margin: calc(3 * var(--margin-y)) auto;
   display: grid;
   grid-template-rows: repeat(3, auto);
+  grid-template-areas:
+    "main"
+    "ratings"
+    "testimonials";
+
+  section {
+    padding: var(--padding-y) 0;
+  }
 `;
