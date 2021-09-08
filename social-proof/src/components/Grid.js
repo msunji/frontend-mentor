13a8c6ae8209d7a1b338879f8b6d6db532228ebf
Marge <marge.consunji@gmail.com>
2021-09-08 17:14:40 +0800

    Change from mobile to desktop layout with grid

--- social-proof/src/components/Grid.js
+++ social-proof/src/components/Grid.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
+import breakpoints from "../styles/breakpoints";
 
 export const Grid = styled.main`
   width: 85%;
-  max-width: 900px;
+  max-width: 1100px;
   margin: calc(3 * var(--margin-y)) auto;
+  padding: 0 var(--padding-x);
   display: grid;
+  gap: 3rem;
   grid-template-rows: repeat(3, auto);
   grid-template-areas:
     "main"
@@ -13,5 +16,13 @@ export const Grid = styled.main`
 
   section {
     padding: var(--padding-y) 0;
+  }
+
+  @media screen and ${breakpoints.lg} {
+    grid-template-columns: 1.4fr 2fr;
+    grid-template-rows: repeat(2, auto);
+    grid-template-areas:
+      "main ratings"
+      "testimonials testimonials";
   }
 `;
