b81c2c303375d46f1b9ea054d2b7cba87048f176
Marge <marge.consunji@gmail.com>
2021-09-13 13:45:32 +0800

    Imported GlobalStyle

--- sunnyside-agency-landing/src/App.js
+++ sunnyside-agency-landing/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import GlobalStyle from './styles/globalStyles';
 import { Header, Footer } from './components/layout';
 import { LandingGrid, Testimonials } from './components';
 
 const App = () => {
   return (
-    <> 
+    <>
+      <GlobalStyle />
       <Header />
       <main>
-      <LandingGrid />
-      <Testimonials />
+        <LandingGrid />
+        <Testimonials />
       </main>
       <Footer />
     </>
-  )
-}
+  );
+};
 
 export default App;
