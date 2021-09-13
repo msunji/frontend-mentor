527e626ec13fc31e12986a241a3eb95838306f80
Marge <marge.consunji@gmail.com>
2021-09-13 13:59:28 +0800

    Added auto margins for left and right to centre text

---
+++ sunnyside-agency-landing/src/components/layout/Container.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 80%;
+  max-width: 900px;
+  margin-left: auto;
+  margin-right: auto;
+`;
