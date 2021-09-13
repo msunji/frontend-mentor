0ffb18a910081dd34562c483afa574317f2be9a1
Marge <marge.consunji@gmail.com>
2021-09-13 15:02:26 +0800

    Started styling logo and footer text

--- sunnyside-agency-landing/src/components/layout/Footer.js
+++ sunnyside-agency-landing/src/components/layout/Footer.js
@@ -1,22 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
+import Logo from '../../assets/img/logo.svg';
+import { Container } from './Container';
+
+const StyledFooter = styled.footer`
+  padding: var(--padding-y) 0;
+  background: var(--cyan);
+  color: var(--cyan-dark-moderate);
+
+  .footer-logo {
+    path {
+      fill: var(--cyan-dark-moderate);
+    }
+  }
+`;
+
+const FooterLinks = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+
+const SocialMediaLinks = styled.ul``;
 
 export const Footer = () => {
   return (
-    <footer>
-      <div>logo</div>
-      <ul>
-        <li>About</li>
-        <li>Services</li>
-        <li>Projects</li>
-        <li>Contact</li>
-      </ul>
-      <div className="attribution">
-        Challenge by{' '}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Your Name Here</a>.
-      </div>
-    </footer>
+    <StyledFooter>
+      <Container>
+        <div>
+          <Logo className="footer-logo" />
+        </div>
+        <FooterLinks>
+          <li>About</li>
+          <li>Services</li>
+          <li>Projects</li>
+        </FooterLinks>
+        <div className="attribution">
+          Challenge by{' '}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Your Name Here</a>.
+        </div>
+      </Container>
+    </StyledFooter>
   );
 };
