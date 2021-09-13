ff546eea4dc472c34e29dd840a491f81c80bd132
Marge <marge.consunji@gmail.com>
2021-09-13 13:14:10 +0800

    Started new project. Added base files

---
+++ sunnyside-agency-landing/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>  
+      <h1>Test</h1>
+      <p>Test</p>
+    </>
+  )
+}
+
+export default App;
---
+++ sunnyside-agency-landing/src/components/layout/Header.js
@@ -0,0 +1,26 @@
+
+export const Header = () => {
+  return (
+    <header>
+      <nav>
+        <div>
+          logo
+        </div>
+        <ul>
+          <li>
+            About
+          </li>
+          <li>
+            Services
+          </li>
+          <li>
+            Projects
+          </li>
+          <li>
+            Contact
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
