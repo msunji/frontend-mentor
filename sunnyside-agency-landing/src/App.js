ada32c0bb9b1c79491c4f75711a8233cef2c7037
Marge <marge.consunji@gmail.com>
2021-09-13 13:59:54 +0800

    Renamed Nav component, added flex alignment

--- sunnyside-agency-landing/src/App.js
+++ sunnyside-agency-landing/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import GlobalStyle from './styles/globalStyles';
-import { Header, Footer } from './components/layout';
+import { Nav, Footer } from './components/layout';
 import { LandingGrid, Testimonials } from './components';
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Nav />
       <main>
         <LandingGrid />
         <Testimonials />
---
+++ sunnyside-agency-landing/src/components/layout/Nav.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Container } from './Container';
+
+const NavContainer = styled(Container)`
+  width: 90%;
+  max-width: 1200px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  ul {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export const Nav = () => {
+  return (
+    <nav>
+      <NavContainer>
+        <div>
+          logo
+        </div>
+        <ul>
+          <li>
+            About
+          </li>
+          <li>
+            Services
+          </li>
+          <li>
+            Projects
+          </li>
+          <li>
+            Contact
+          </li>
+        </ul>
+      </NavContainer>
+    </nav>
+  )
+}
