d330bb2ce246e0d124fc820e23b97b1c1bc27e30
Marge <marge.consunji@gmail.com>
2021-09-14 15:15:55 +0800

    Added Hero component placeholder text

---
+++ sunnyside-agency-landing/src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Container } from './layout';
+
+export const Hero = () => {
+  return (
+    <section>
+      <Container>
+        <h1>We are Creatives</h1>
+      </Container>
+    </section>
+  )
+}
