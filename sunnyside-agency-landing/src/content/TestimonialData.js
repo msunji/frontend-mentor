8d97b06e70b31cd2f8772ff6961cfd124dd95959
Marge <marge.consunji@gmail.com>
2021-09-14 15:37:48 +0800

    Added image paths

--- sunnyside-agency-landing/src/content/TestimonialData.js
+++ sunnyside-agency-landing/src/content/TestimonialData.js
@@ -1,17 +1,19 @@
 export const TestimonialData = [
   {
     "name": "Emily R.",
-    "photo": "/",
+    "photo": "/public/img/testimonials/image-emily.jpg",
     "title": "Marketing Director",
     "testimonial": "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit." 
   },
   {
     "name": "Thomas S.",
+    "photo": "/public/img/testimonials/image-thomas.jpg",
     "title": "Chief Operating Officer",
     "testimonial": "Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience." 
   },
   {
     "name": "Jennie F.",
+    "photo": "/public/img/testimonials/image-jennie.jpg",
     "title": "Business Owner",
     "testimonial": "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!" 
   }
