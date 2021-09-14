68110476d7af97aad1dc2e6a312347624a96b14a
Marge <marge.consunji@gmail.com>
2021-09-14 15:36:12 +0800

    Added placeholder bg colour

--- sunnyside-agency-landing/src/components/Hero.js
+++ sunnyside-agency-landing/src/components/Hero.js
@@ -2,12 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 import { Container } from './layout';
 
+const HeroSection = styled.section`
+  background: var(--bright-blue);
+`;
+
 export const Hero = () => {
   return (
-    <section>
+    <HeroSection>
       <Container>
         <h1>We are Creatives</h1>
       </Container>
-    </section>
+    </HeroSection>
   )
 }
