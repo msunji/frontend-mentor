91d5cf8fe5815b60594ae5450807e0888f80e1bc
Marge <marge.consunji@gmail.com>
2021-09-14 15:43:54 +0800

    Imported images

--- sunnyside-agency-landing/src/content/TestimonialData.js
+++ sunnyside-agency-landing/src/content/TestimonialData.js
@@ -1,19 +1,23 @@
+import Emily from '/public/img/testimonials/image-emily.jpg';
+import Thomas from '/public/img/testimonials/image-thomas.jpg';
+import Jennie from '/public/img/testimonials/image-jennie.jpg';
+
 export const TestimonialData = [
   {
     "name": "Emily R.",
-    "photo": "/public/img/testimonials/image-emily.jpg",
+    "photo": Emily,
     "title": "Marketing Director",
     "testimonial": "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit." 
   },
   {
     "name": "Thomas S.",
-    "photo": "/public/img/testimonials/image-thomas.jpg",
+    "photo": Thomas,
     "title": "Chief Operating Officer",
     "testimonial": "Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience." 
   },
   {
     "name": "Jennie F.",
-    "photo": "/public/img/testimonials/image-jennie.jpg",
+    "photo": Jennie,
     "title": "Business Owner",
     "testimonial": "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!" 
   }
