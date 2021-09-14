426cb40a1a1f0337a9d181283d72ec98fe0c38dc
Marge <marge.consunji@gmail.com>
2021-09-14 15:14:42 +0800

    Added social media icons, sorted out flex alignment for links and added attribution section

--- sunnyside-agency-landing/src/components/layout/Footer.js
+++ sunnyside-agency-landing/src/components/layout/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Logo from '../../assets/img/logo.svg';
+import { FaFacebookSquare, FaInstagram, FaTwitter, FaPinterest } from 'react-icons/fa';
 import { Container } from './Container';
 
 const StyledFooter = styled.footer`
@@ -9,15 +10,50 @@ const StyledFooter = styled.footer`
   color: var(--cyan-dark-moderate);
 
   .footer-logo {
+    transform: scale(1.2);
     path {
-      fill: var(--cyan-dark-moderate);
+      fill: var(--cyan-dark-desat);
+      opacity: 0.85;
     }
+    margin-bottom: 1.2rem;
   }
+
+  .attribution {
+    font-size: 0.9em;
+  }
+`;
+
+const FooterContainer = styled(Container)`
+  padding: var(--padding-y);
+  text-align: center;
 `;
 
 const FooterLinks = styled.ul`
   display: flex;
   align-items: center;
+  justify-content: center;
+`;
+
+const FooterNavLinks = styled(FooterLinks)`
+  margin-bottom: 4rem;
+  color: var(--cyan-dark-moderate);
+  li:not(:last-of-type) {
+    margin-right: 4rem;
+  }
+`;
+
+const FooterSocMedLinks = styled(FooterLinks)`
+  margin-bottom: 4rem;
+  svg {
+    transform: scale(1.4);
+    path {
+      fill: var(--cyan-dark-desat);
+      opacity: 0.85;
+    }
+  }
+  li:not(:last-of-type) {
+    margin-right: 2.1rem;
+  }
 `;
 
 const SocialMediaLinks = styled.ul``;
@@ -25,23 +61,30 @@ const SocialMediaLinks = styled.ul``;
 export const Footer = () => {
   return (
     <StyledFooter>
-      <Container>
+      <FooterContainer>
         <div>
           <Logo className="footer-logo" />
         </div>
-        <FooterLinks>
+        <FooterNavLinks>
           <li>About</li>
           <li>Services</li>
           <li>Projects</li>
-        </FooterLinks>
+        </FooterNavLinks>
+        <FooterSocMedLinks>
+          <li><FaFacebookSquare /></li>
+          <li><FaInstagram /></li>
+          <li><FaTwitter /></li>
+          <li><FaPinterest /></li>
+        </FooterSocMedLinks>
         <div className="attribution">
           Challenge by{' '}
           <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
             Frontend Mentor
           </a>
-          . Coded by <a href="#">Your Name Here</a>.
+          .<br />
+          Coded by <a href="#">Your Name Here</a>.
         </div>
-      </Container>
+      </FooterContainer>
     </StyledFooter>
   );
 };
