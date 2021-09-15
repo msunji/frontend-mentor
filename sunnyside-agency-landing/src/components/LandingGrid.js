2a49ec454b74c47c225beb20de87b115974c3553
Marge <marge.consunji@gmail.com>
2021-09-15 17:03:09 +0800

    Opted for aspect ratio instead of padding hack

--- sunnyside-agency-landing/src/components/LandingGrid.js
+++ sunnyside-agency-landing/src/components/LandingGrid.js
@@ -11,29 +11,37 @@ import CupBgMobile from '/public/img/mobile/image-stand-out.jpg';
 import CherryBgMobile from '/public/img/mobile/image-graphic-design.jpg';
 import OrangeBgMobile from '/public/img/mobile/image-photography.jpg';
 
+// Import breakpoints
+import breakpoints from '../styles/breakpoints';
+
 const LandingGridSection = styled.section`
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows: 312px min-content 312px min-content repeat(2, minmax(33.3em, 1fr));
+  grid-template-rows: 312px min-content 312px min-content repeat(
+      2,
+      minmax(33.3em, 1fr)
+    );
   grid-remplate-rows: repeat(6, 1fr);
-  grid-template-areas: 
-    "egg"
-    "transform"
-    "cup"
-    "stand-out"
-    "cherry"
-    "orange";
+  grid-template-areas:
+    'egg'
+    'transform'
+    'cup'
+    'stand-out'
+    'cherry'
+    'orange';
 
-  // grid-template-rows: repeat(3, 1fr);
-  // grid-template-columns: repeat(2, 1fr);
+  @media screen and ${breakpoints.lg} {
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    grid-template-areas:
+      'transform egg'
+      'cup stand-out'
+      'cherry orange';
+  }
 `;
 
 const GridSquare = styled.div`
-  // position: relative;
-  // padding-bottom: 83%;
-  // height: 0;
-
-  grid-area: ${props => props.gridName};
+  grid-area: ${(props) => props.gridName};
 
   h2 {
     color: var(--blue-black);
@@ -49,23 +57,17 @@ const GridSquare = styled.div`
     margin-bottom: 1.8rem;
   }
 
-  .content-container {
-    // position: absolute;
-    // width: 100%;
-    // height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: calc(2*var(--padding-y)) 0;
-
-    .content {
-      width: 80%;
-      text-align: center;
-    }
+  .grid-content {
+    display: block;
+    padding: var(--padding-y) var(--padding-x);
   }
+
   // aspect-ratio property works a treat, but has no IE support
   // aspect-ratio: 1.2;
+
+  @media screen and ${breakpoints.lg} {
+    aspect-ratio: 6 / 5;
+  }
 `;
 
 const GridSquareBg = styled(GridSquare)`
@@ -74,8 +76,10 @@ const GridSquareBg = styled(GridSquare)`
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+  display: flex;
+  align-items: center;
 
-  @media screen and (min-width: 600px) {
+  @media screen and ${breakpoints.lg} {
     background: ${(props) => `var(--${props.color})`}
       url(${(props) => props.desktop});
     background-size: cover;
@@ -83,35 +87,36 @@ const GridSquareBg = styled(GridSquare)`
 `;
 
 const GridSquareBgText = styled(GridSquareBg)`
-  .bgtext-content-container {
-    // position: absolute;
-    // top: 0;
-    // left: 0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: flex-end;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
 
-    .bgtext-content {
-      width: 60%;
-      text-align: center;
-      padding-bottom: 1.5rem;
+  .bgtext-content {
+    width: 60%;
+    text-align: center;
+    padding-bottom: 1.5rem;
 
-      h3 {
-        font-size: 1.5em;
-        letter-spacing: -0.2px;
-        line-height: 1.25;
-        margin-bottom: 1.2rem;
-        color: ${(props) => `var(--${props.headerColor})`};
-      }
-      p {
-        color: ${(props) => `var(--${props.textColor})`};
-        opacity: 0.7;
-        line-height: 1.7;
-        font-size: 0.88em;
-        letter-spacing: -0.11px;
-      }
+    h3 {
+      font-size: 1.5em;
+      letter-spacing: -0.2px;
+      line-height: 1.25;
+      margin-bottom: 1.2rem;
+      color: ${(props) => `var(--${props.headerColor})`};
+    }
+    p {
+      color: ${(props) => `var(--${props.textColor})`};
+      opacity: 0.7;
+      line-height: 1.7;
+      font-size: 0.88em;
+      letter-spacing: -0.11px;
+    }
+  }
+
+  @media screen and ${breakpoints.lg} {
+    .bgtext-content-container {
+      position: absolute;
+      top: 0;
+      left: 0;
     }
   }
 `;
@@ -129,7 +134,6 @@ const GridLink = styled.a`
 
   &:hover,
   &:active {
-
     &::before {
       opacity: 0.25;
       transition: opacity 1s ease;
@@ -149,7 +153,7 @@ const GridLink = styled.a`
     width: 108%;
     height: 10px;
     z-index: -2;
-    background: ${props => `var(--${props.color})`};
+    background: ${(props) => `var(--${props.color})`};
     opacity: 0.25;
     border-radius: 28px;
     transition: opacity 0.4s ease;
@@ -163,7 +167,7 @@ const GridLink = styled.a`
     width: 0%;
     height: 10px;
     z-index: -1;
-    background: ${props => `var(--${props.color})`};
+    background: ${(props) => `var(--${props.color})`};
     opacity: 1;
     border-radius: 28px;
     transition: all 0.4s ease;
@@ -174,20 +178,20 @@ export const LandingGrid = () => {
   return (
     <LandingGridSection>
       <GridSquare gridName="transform">
-        <div className="content-container">
-          <div className="content">
-            <h2>
-              Transform your
-              <br />
-              brand
-            </h2>
-            <p>
-              We are a full-service creative agency specializing in helping
-              brands grow fast. Engage your clients through compelling visuals
-              that do most of the marketing for you.
-            </p>
-            <GridLink color="yellow" href="#">Learn More</GridLink>
-          </div>
+        <div className="grid-content">
+          <h2>
+            Transform your
+            <br />
+            brand
+          </h2>
+          <p>
+            We are a full-service creative agency specializing in helping brands
+            grow fast. Engage your clients through compelling visuals that do
+            most of the marketing for you.
+          </p>
+          <GridLink color="yellow" href="#">
+            Learn More
+          </GridLink>
         </div>
       </GridSquare>
       <GridSquareBg
@@ -203,20 +207,20 @@ export const LandingGrid = () => {
         mobile={CupBgMobile}
       ></GridSquareBg>
       <GridSquare gridName="stand-out">
-        <div className="content-container">
-          <div className="content">
-            <h2>
-              Stand out to the right
-              <br />
-              audience
-            </h2>
-            <p>
-              Using a collaborative formula of designers, researchers,
-              photographers, videographers, and copywriters, we’ll build and
-              extend yourbrand in digital places.
-            </p>
-            <GridLink color="red-soft" href="#">Learn More</GridLink>
-          </div>
+        <div className="grid-content">
+          <h2>
+            Stand out to the right
+            <br />
+            audience
+          </h2>
+          <p>
+            Using a collaborative formula of designers, researchers,
+            photographers, videographers, and copywriters, we’ll build and
+            extend yourbrand in digital places.
+          </p>
+          <GridLink color="red-soft" href="#">
+            Learn More
+          </GridLink>
         </div>
       </GridSquare>
       <GridSquareBgText
@@ -227,15 +231,13 @@ export const LandingGrid = () => {
         desktop={CherryBgDesktop}
         mobile={CherryBgMobile}
       >
-        <div className="bgtext-content-container">
-          <div className="bgtext-content">
-            <h3>Graphic Design</h3>
-            <p>
-              Great design makes you memorable. We deliver artwork that
-              underscores your brand message and captures potential clients’
-              attention.
-            </p>
-          </div>
+        <div className="bgtext-content">
+          <h3>Graphic Design</h3>
+          <p>
+            Great design makes you memorable. We deliver artwork that
+            underscores your brand message and captures potential clients’
+            attention.
+          </p>
         </div>
       </GridSquareBgText>
       <GridSquareBgText
@@ -246,14 +248,12 @@ export const LandingGrid = () => {
         desktop={OrangeBgDesktop}
         mobile={OrangeBgMobile}
       >
-        <div className="bgtext-content-container">
-          <div className="bgtext-content">
-            <h3>Photography</h3>
-            <p>
-              Increase your credibility by getting the most stunning,
-              high-quality photos that improve your business image.
-            </p>
-          </div>
+        <div className="bgtext-content">
+          <h3>Photography</h3>
+          <p>
+            Increase your credibility by getting the most stunning, high-quality
+            photos that improve your business image.
+          </p>
         </div>
       </GridSquareBgText>
     </LandingGridSection>
