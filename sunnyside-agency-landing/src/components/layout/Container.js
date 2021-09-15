9c71b839646c843322a8366a24618c353fc750ba
Marge <marge.consunji@gmail.com>
2021-09-15 13:35:55 +0800

    Made max width wider

--- sunnyside-agency-landing/src/components/layout/Container.js
+++ sunnyside-agency-landing/src/components/layout/Container.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 80%;
-  max-width: 1100px;
+  max-width: 1400px;
   margin-left: auto;
   margin-right: auto;
 `;
