9e991bf3095e8857b14fc43c9f65781518d15351
Marge <marge.consunji@gmail.com>
2022-05-13 10:50:24 +0800

    Move social proof project to own repo

--- social-proof/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { Ratings } from "./Ratings";
-export { Testimonials } from "./Testimonials";
-export { MainText } from "./MainText";
-export { Grid } from "./Grid";
-export { Footer } from "./Footer";
--- social-proof/src/styles/breakpoints.js
+++
@@ -1,8 +0,0 @@
-const breakpoints = {
-  xs: "(min-width: 600px)",
-  sm: "(min-width: 768px)",
-  lg: "(min-width: 780px)",
-  xl: "(min-width: 850px)",
-};
-
-export default breakpoints;
